0c77ac2a3cc36678fd824755490f5ee3a500f40b
eddy <edwardquintero@protonmail.com>
2024-08-28 18:08:48 -0400

    upload gitsync file

---
+++ gitsync.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: directory where google drive is synced
+set "BACKUP_DIR=C:\path\to\GoogleDrive\Backups"
+
+:: replace with your github Personal Access Token
+set "GITHUB_TOKEN=your_personal_access_token"
+
+:: Github repo URL (replace with your own)
+set "GITHUB_REPO=https://%GITHUB_TOKEN%@github.com/username/repository.git"
+
+:: Sets time interval in seconds for backup. Modify to whatever you want.
+set "INTERVAL=3600"
+
+:: Start the loop for automatic backups
+:LOOP
+echo Backing up the repository...
+
+:: Change to the repository directory
+cd /d "%REPO_DIR%"
+
+:: Pull the latest changes from the GitHub repository using the token for authentication
+git pull %GITHUB_REPO%
+
+:: copies repository to google drive
+xcopy "%REPO_DIR%" "%BACKUP_DIR%\repository_backup" /e /y /i
+
+:: wait for next backup
+timeout /t %INTERVAL% /nobreak
+
+:: repeats loop
+goto LOOP
